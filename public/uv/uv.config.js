4d9d6e56ca136475e61b92ecd851e930a4add6e4
SwissCh33se <151626990+SwissCh33se@users.noreply.github.com>
2026-02-24 20:56:03 -0500

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: 'https://bare.benroxy.me/', // A fast public bare for YouTube
+    bare: 'https://bare.benroxy.me/', // This fast server handles the "heavy" YouTube data
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
