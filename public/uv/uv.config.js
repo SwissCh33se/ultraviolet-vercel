f068f4ef0db0a9e20519d44a1dc5e0c4630faea2
SwissCh33se <151626990+SwissCh33se@users.noreply.github.com>
2026-02-24 20:44:47 -0500

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,12 +1,11 @@
-/*global Ultraviolet*/
 self.__uv$config = {
-    prefix: "/uv/",
-    bare: 'https://bare.benroxy.me/', // This is a common fast public bare
+    prefix: '/uv/service/',
+    bare: 'https://bare.benroxy.me/', // A fast public bare for YouTube
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: "/uv/uv.handler.js",
-    client: "/uv/uv.client.js",
-    bundle: "/uv/uv.bundle.js",
-    config: "/uv/uv.config.js",
-    sw: "/uv/uv.sw.js",
+    handler: '/uv/uv.handler.js',
+    client: '/uv/uv.client.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
 };
